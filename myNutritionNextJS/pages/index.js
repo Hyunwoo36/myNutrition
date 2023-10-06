d08cb02843413642ca81c4988c7cd96f23196893
Harry CHOI <h2choi@ucsd.edu>
2023-10-06 15:37:23 -0700

    firt commit

---
+++ myNutritionNextJS/pages/index.js
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.mainContainer}>
+      <Head>
+        <title>MyNutritionist</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div>
+          <div>icon</div>
+          <div>Nutritionist</div>
+        </div>
+        <div className={styles.headNavigation}>
+          <div>
+            Options
+          </div>
+          <div>
+            Options
+          </div>
+          <div>
+            Options
+          </div>
+          <div>
+            Options
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <h1>Footer contianer</h1>
+      </footer>
+
+      
+    </div>
+  );
+}
