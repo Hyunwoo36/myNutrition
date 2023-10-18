ef5ebdc1a9bd6a3937f6ac0a347a59fc421c8e23
Harry CHOI <h2choi@ucsd.edu>
2023-10-17 19:06:58 -0700

    fetching api

--- myNutritionNextJS/pages/index.js
+++ myNutritionNextJS/pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import axios from 'axios';
 
-export default function Home() {
+export default function Home({recipeData}) {
 
   return (
     <div className={styles.mainContainer}>
@@ -31,7 +32,10 @@ export default function Home() {
             </div>
           </div>
         </div>
-
+      <div>
+        fetching data:
+        <pre>{JSON.stringify(recipeData, null, 2)}</pre>
+      </div>
       </main>
 
       <footer>
@@ -41,4 +45,24 @@ export default function Home() {
 
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const apiKey = 'f88ad23cb0f049e684c0a5bf244fa473';
+
+  try {
+    const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2&apiKey=${apiKey}`);
+    return {
+      props: {
+        recipeData: response.data
+      }
+    }
+  } catch (error) {
+    console.error("Error fetching data from Spoonacular API:", error);
+    return {
+      props: {
+        recipeData: {}
+      }
+    }
+  }
 }
